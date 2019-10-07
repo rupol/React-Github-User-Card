184640ddc6888e7c9479082794090a7638419dc7
Ruth Poliakon <r.poliakon@gmail.com>
2019-10-06 18:37:34 -0600

    user data fetch and card styling

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -16,36 +16,84 @@ class App extends React.Component {
     this.fetchGithubData();
   }
 
+  // fetchGithubData = () => {
+  //   axios
+  //     .get("https://api.github.com/users/rupol/following")
+  //     .then(response => {
+  //       // network request resolved
+  //       // create following array from response data
+  //       this.setState({
+  //         users: response.data
+  //       });
+  //       console.log(this.state.users);
+  //     })
+  //     .catch(error => {
+  //       // network request rejected
+  //       console.log("Network request was unsuccessful");
+  //       console.log(error);
+  //     });
+  // };
+
   fetchGithubData = () => {
     axios
       .get("https://api.github.com/users/rupol/following")
       .then(response => {
-        // network request resolved
         // create following array from response data
-        this.setState({
-          users: response.data
+        const followingArray = response.data;
+
+        // create array of following urls
+        const followingUrl = followingArray.map(item => {
+          return item.url;
         });
-        console.log(this.state.users);
+
+        // iterate over following urls
+        followingUrl.map(item => {
+          axios
+            // requesting data for each user
+            .get(item)
+            .then(response => {
+              const newUser = {
+                id: response.data.id,
+                imageUrl: response.data.avatar_url,
+                name: response.data.name,
+                login: response.data.login,
+                location: response.data.location,
+                profileUrl: response.data.html_url,
+                followers: response.data.followers,
+                following: response.data.following,
+                bio: response.data.bio
+              };
+              this.setState({
+                users: [...this.state.users, newUser]
+              });
+            })
+            .catch(error => {
+              console.log("Users Network request was unsuccessful");
+              console.log(error);
+            });
+        });
       })
       .catch(error => {
-        // network request rejected
-        console.log("Network request was unsuccessful");
+        console.log("Following Network request was unsuccessful");
         console.log(error);
       });
   };
 
   render() {
     return (
-      <>
-        <img src="./assets/lambdalogo.png" alt="Lambda Logo" />
-        <p>❤️'s</p>
-        <img src="./assets/githublogo.png" alt="GitHub Logo" />
-        <div>
+      <div class="container">
+        <div class="header">
+          <img src="./assets/lambdalogo.png" alt="Lambda Logo" />
+          <p>❤️'s</p>
+          <img src="./assets/githublogo.png" alt="GitHub Logo" />
+        </div>
+        <div class="cards">
+          {console.log(this.state.users)}
           {this.state.users.map(user => (
             <GitCard key={user.id} user={user} />
           ))}
         </div>
-      </>
+      </div>
     );
   }
 }
--- github-user-card/src/components/GitCard.js
+++ github-user-card/src/components/GitCard.js
@@ -3,9 +3,18 @@ import React from "react";
 const GitCard = props => {
   return (
     <div className="card">
-      <img src={props.user.avatar_url} />
+      <img src={props.user.imageUrl} alt={props.user.name} />
       <div className="card-info">
-        <h3 className="name">{props.user.login}</h3>
+        <h3 className="name">{props.user.name}</h3>
+        <p className="username">{props.user.login}</p>
+        <p>Location: {props.user.location}</p>
+        <p>
+          Profile:
+          <a href={props.user.profileUrl}>{props.user.profileUrl}</a>
+        </p>
+        <p>Followers: {props.user.followers}</p>
+        <p>Following: {props.user.following}</p>
+        <p>Bio: {props.user.bio}</p>
       </div>
     </div>
   );
